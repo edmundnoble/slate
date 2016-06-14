6dfebda988644d37a458e69eeb9ddcc1d3d17daf
Edmund Noble <edmundnoble@gmail.com>
2016-06-13 20:40:26 -0400

    Two more compilation errors to go

--- qq/shared/src/main/scala/qq/QQCompiler.scala
+++ qq/shared/src/main/scala/qq/QQCompiler.scala
@@ -1,46 +1,70 @@
 package qq
 
 import qq.Util._
-import qq.QQCompiler.{NoSuchMethod, QQCompilationException, QQRuntimeException}
+import qq.QQCompiler.{NoSuchMethod, QQCompilationException, QQRuntimeException, WrongNumParams}
 import qq.Definition
 import qq.QQFilterComponent._
-
 import monix.eval.{Coeval, Task}
+
 import scalaz.std.list._
 import scalaz.{EitherT, \/}
 import matryoshka._
 import Recursive.ops._
 import Corecursive.ops._
 import TraverseT.ops._
+import monocle.macros.GenLens
+import shapeless._
+import ops.hlist.{Length, Mapped}
+import shapeless.LUBConstraint.<<:
+import shapeless.ops.nat.ToInt
+import shapeless.syntax.sized._
+
 import scalaz.syntax.either._
 import scalaz.syntax.std.option._
 import scalaz.syntax.traverse._
-import scalaz.syntax.monad._
 
 abstract class QQCompiler {
 
   type AnyTy
   type CompiledFilter = AnyTy => Task[List[AnyTy]]
   type OrCompilationError[T] = QQCompilationException \/ T
-
-  trait QQPrelude {
-    def length: CompiledDefinition
-    def keys: CompiledDefinition
-    def arrays: CompiledDefinition
-    def objects: CompiledDefinition
-    def iterables: CompiledDefinition
-    def booleans: CompiledDefinition
-    def numbers: CompiledDefinition
-    def strings: CompiledDefinition
-    def nulls: CompiledDefinition
-    def values: CompiledDefinition
-    def scalars: CompiledDefinition
-
-    def all: List[CompiledDefinition] =
-      List(length, keys, arrays, objects, iterables, booleans, numbers, strings, nulls, values, scalars)
+  sealed abstract class CDefBase {
+    type Aux <: Nat
   }
 
-  case class CompiledDefinition(name: String, params: List[String], body: CompiledFilter)
+  case class CompiledDefinition[
+  N <: Nat
+  ](name: String, body: Sized[List[CompiledFilter], N] => QQCompilationException \/ CompiledFilter)(implicit val numParams: ToInt[N]) extends CDefBase {
+    override type Aux = N
+  }
+
+  trait QQPrelude {
+    def length: CompiledDefinition[_0]
+
+    def keys: CompiledDefinition[_0]
+
+    def arrays: CompiledDefinition[_0]
+
+    def objects: CompiledDefinition[_0]
+
+    def iterables: CompiledDefinition[_0]
+
+    def booleans: CompiledDefinition[_0]
+
+    def numbers: CompiledDefinition[_0]
+
+    def strings: CompiledDefinition[_0]
+
+    def nulls: CompiledDefinition[_0]
+
+    def values: CompiledDefinition[_0]
+
+    def scalars: CompiledDefinition[_0]
+
+    def all: List[CompiledDefinition[_]] =
+      length :: keys :: arrays :: objects :: iterables :: booleans ::
+        numbers :: strings :: nulls :: values :: scalars :: Nil
+  }
 
   def composeCompiledFilters(firstFilter: CompiledFilter, secondFilter: CompiledFilter): CompiledFilter = { jsv: AnyTy =>
     for {
@@ -53,18 +77,34 @@ abstract class QQCompiler {
     Task.sequence(functions.map(_ (jsv))).map(_.flatten)
   }
 
-  def compileProgram(definitions: List[Definition], main: QQFilter): QQCompilationException \/ CompiledFilter = {
-    val compiledDefinitions: QQCompilationException \/ List[CompiledDefinition] =
-      definitions.foldLeft(nil[CompiledDefinition].right[QQCompilationException]) {
-        (soFar, nextDefinition) =>
-          soFar.flatMap(definitionsSoFar =>
-            compile(definitionsSoFar, nextDefinition.body)
-              .map(CompiledDefinition(nextDefinition.name, nextDefinition.params, _) :: definitionsSoFar))
+  import ops.hlist.Comapped
+
+  object foldfun extends Poly2 {
+    implicit def st0[In0 <: HList, N <: Nat](implicit ev: UnaryTCConstraint[In0, CompiledDefinition]) =
+      at[\/[QQCompilationException, List[CompiledDefinition[_]]], Definition[N]] { (soFar, nextDefinition) =>
+        soFar.map(definitionsSoFar =>
+          CompiledDefinition[nextDefinition.Aux](nextDefinition.name, (params: Sized[List[CompiledFilter], N]) => {
+            val paramsAsDefinitions = (nextDefinition.params.unsized, params.unsized).zipped.map((name, value) => CompiledDefinition[_0](name, _ => value.right))
+            compile(definitionsSoFar ++ paramsAsDefinitions, nextDefinition.body)
+          })(nextDefinition.numParams)
+        )
       }
-    compiledDefinitions.flatMap(compile(_, main))
   }
 
-  def compileStep(definitions: List[CompiledDefinition], filter: QQFilterComponent[CompiledFilter]): OrCompilationError[CompiledFilter] = filter match {
+  def compileProgram[NS <: HList, L <: HList](definitions: L, main: QQFilter)(implicit ev: Comapped.Aux[L, Definition, NS],
+                                                                                      ev2: LUBConstraint[NS, Nat]): QQCompilationException \/ CompiledFilter = {
+    val compiledDefinitions: List[CompiledDefinition[_]] =
+      definitions.foldLeft(nil[CompiledDefinition[_]].right[QQCompilationException])(foldfun)
+    compile(compiledDefinitions, main)
+  }
+
+  class findDefinition(name: String) extends Poly2 {
+    implicit def st0 = at[Option[CompiledDefinition[_]], CompiledDefinition[_]] { (maybe, next) =>
+      maybe.orElse(if (next.name == name) Some(next) else None)
+    }
+  }
+
+  def compileStep(definitions: List[CompiledDefinition[_]], filter: QQFilterComponent[CompiledFilter]): OrCompilationError[CompiledFilter] = filter match {
     case IdFilter() => ((jsv: AnyTy) => Task.now(jsv :: Nil)).right
     case ComposeFilters(f, s) => composeCompiledFilters(f, s).right
     case EnlistFilter(f) => enlistFilter(f).right
@@ -76,10 +116,20 @@ abstract class QQCompiler {
     case SelectRange(s, e) => selectRange(s, e).right
     case SelectIndex(i) => selectIndex(i).right
     case CallFilter(filterIdentifier, params) =>
-      definitions.find(_.name == filterIdentifier).cata(_.body.right, NoSuchMethod(filterIdentifier).left)
+      definitions.find(_.name == filterIdentifier).cata(
+        { (defn: CompiledDefinition[_]) =>
+          params.sized[defn.Aux](defn.numParams).cata({
+            verifiedParams =>
+              defn.body(verifiedParams)
+          },
+            WrongNumParams(filterIdentifier, defn.numParams(), params.length).left
+          )
+        },
+        NoSuchMethod(filterIdentifier).left
+      )
   }
 
-  def compile(definitions: List[CompiledDefinition], filter: QQFilter): OrCompilationError[CompiledFilter] = {
+  def compile(definitions: List[CompiledDefinition[_]], filter: QQFilter): OrCompilationError[CompiledFilter] = {
     filter.cataM[OrCompilationError, CompiledFilter](compileStep(prelude.all ++ definitions, _))
   }
 
@@ -96,6 +146,9 @@ object QQCompiler {
 
   class QQRuntimeException(message: String) extends RuntimeException(message)
   class QQCompilationException(message: String) extends RuntimeException(message)
-  case class NoSuchMethod(name: String) extends QQCompilationException(message = s"No such method: $name")
+  case class NoSuchMethod(name: String)
+    extends QQCompilationException(message = s"No such method: $name")
+  case class WrongNumParams(name: String, correct: Int, you: Int)
+    extends QQCompilationException(message = s"Wrong number of params for filter $name: passed $you, wanted $correct")
 
 }
--- qq/shared/src/main/scala/qq/QQFilter.scala
+++ qq/shared/src/main/scala/qq/QQFilter.scala
@@ -1,15 +1,16 @@
 package qq
 
-import monocle.macros._
-
 import scala.language.higherKinds
 import scalaz.\/
 import matryoshka._
+import shapeless._
+import shapeless.ops.nat.ToInt
 
-final case class Definition(name: String, params: List[String], body: QQFilter)
-
-object Definition {
-  val body = GenLens[Definition](_.body)
+final case class Definition[N <: Nat](name: String,
+                                      params: Sized[List[String], N],
+                                      body: QQFilter)
+                                     (implicit val numParams: ToInt[N]) {
+  type Aux = N
 }
 
 object QQFilter {
@@ -24,7 +25,7 @@ object QQFilter {
   def collectResults(f: QQFilter): QQFilter = Fix(CollectResults(f))
   def ensequence(filters: List[QQFilter]): QQFilter = Fix(EnsequenceFilters(filters))
   def enject(obj: List[((String \/ QQFilter), QQFilter)]): QQFilter = Fix(EnjectFilters(obj))
-  def call(name: String): QQFilter = Fix(CallFilter(name), Nil)
+  def call(name: String): QQFilter = Fix(CallFilter(name, Nil))
   def selectKey(key: String): QQFilter = Fix(SelectKey(key))
   def selectIndex(index: Int): QQFilter = Fix(SelectIndex(index))
   def selectRange(start: Int, end: Int): QQFilter = Fix(SelectRange(start, end))
