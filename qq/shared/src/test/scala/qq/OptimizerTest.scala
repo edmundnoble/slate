7a8c8c2845042dde759663ee3842096403a8d991
Edmund Noble <edmundnoble@gmail.com>
2016-06-19 10:46:34 -0400

    Import all members of filter in OptimizerTest

--- qq/shared/src/test/scala/qq/OptimizerTest.scala
+++ qq/shared/src/test/scala/qq/OptimizerTest.scala
@@ -2,57 +2,50 @@ package qq
 
 import utest._
 import matryoshka._
+import qq.Filter._
 
 object OptimizerTest extends utest.TestSuite with Asserts {
 
   override val tests = TestSuite {
 
     "optimize simple compositions" - {
-      Optimizer.optimize(Filter.compose(Filter.id, Filter.selectKey("key"))) ===>
-        Filter.selectKey("key")
-      Optimizer.optimize(Filter.compose(Filter.id, Filter.compose(Filter.selectKey("key"), Filter.id))) ===>
-        Filter.selectKey("key")
+      Optimizer.optimize(compose(id, selectKey("key"))) ===>
+        selectKey("key")
+      Optimizer.optimize(compose(id, compose(selectKey("key"), id))) ===>
+        selectKey("key")
     }
 
     "optimize collectresults and enlist duality" - {
-      Optimizer.optimize(Filter.collectResults(Filter.enlist(Filter.id))) ===> Filter.id
-      Optimizer.optimize(Filter.enlist(Filter.collectResults(Filter.id))) ===> Filter.id
+      Optimizer.optimize(collectResults(enlist(id))) ===> id
+      Optimizer.optimize(enlist(collectResults(id))) ===> id
     }
 
     "do nested optimizations" - {
       Optimizer.optimize(
-        Filter.collectResults(
-          Filter.compose(
-            Filter.id,
-            Filter.compose(
-              Filter.id,
-              Filter.enlist(Filter.id)
-            )
-          )
-        )
+        collectResults(compose(id, compose(id, enlist(id))))
       ) ===>
-        Filter.id
+        id
     }
 
     "optimize all math in constant expressions" - {
       Optimizer.optimize(
-        Filter.add(
-          Filter.multiply(
-            Filter.constNumber(5.4),
-            Filter.divide(
-              Filter.constNumber(1),
-              Filter.subtract(
-                Filter.constNumber(1),
-                Filter.multiply(
-                  Filter.constNumber(0.25),
-                  Filter.constNumber(2)
+        add(
+          multiply(
+            constNumber(5.4),
+            divide(
+              constNumber(1),
+              subtract(
+                constNumber(1),
+                multiply(
+                  constNumber(0.25),
+                  constNumber(2)
                 )
               )
             )
           ),
-          Filter.constNumber(20))
+          constNumber(20))
       ) ===>
-        Filter.constNumber(20 + (5.4 * (1 / (1 - (0.25 * 2)))))
+        constNumber(20 + (5.4 * (1 / (1 - (0.25 * 2)))))
     }
 
   }
