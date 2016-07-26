48c6fa641d1b28e45401960b909d772e8dfeffd9
Edmund Noble <edmundnoble@gmail.com>
2016-07-26 19:24:17 -0400

    Formatting

--- qq/shared/src/main/scala/qq/SafeRec.scala
+++ qq/shared/src/main/scala/qq/SafeRec.scala
@@ -24,17 +24,22 @@ object SafeRec {
     loop(i)
   }
 
-  def cata[T[_[_]], F[_] : Traverse, A](tf: T[F])(destroy: F[A] => A, maxStackSize: Int = 200)(implicit Re: Recursive[T]): Trampoline[A] = {
-    recLimitStack[T[F], A](tf, (in, loop) => in.project.traverse[Trampoline, A](loop) map destroy, maxStackSize)
+  @inline def cata[T[_[_]], F[_] : Traverse, A](tf: T[F])(destroy: F[A] => A, maxStackSize: Int = 200)
+                                               (implicit Re: Recursive[T]): Trampoline[A] = {
+    recLimitStack[T[F], A](tf,
+      (in, loop) => in.project.traverse[Trampoline, A](loop) map destroy, maxStackSize)
   }
 
-  def cataM[T[_[_]], F[_] : Traverse, M[_] : Monad, A](tf: T[F])(destroy: F[A] => M[A], maxStackSize: Int = 200)(implicit Re: Recursive[T]): Trampoline[M[A]] = {
-    recLimitStack[T[F], M[A]](tf, (in, loop) => in.project.traverse[Trampoline, M[A]](loop) map (_.sequence[M, A].flatMap(destroy)), maxStackSize)
+  @inline def cataM[T[_[_]], F[_] : Traverse, M[_] : Monad, A](tf: T[F])(destroy: F[A] => M[A], maxStackSize: Int = 200)
+                                                              (implicit Re: Recursive[T]): Trampoline[M[A]] = {
+    recLimitStack[T[F], M[A]](tf,
+      (in, loop) => in.project.traverse[Trampoline, M[A]](loop) map (_.sequence[M, A].flatMap(destroy)), maxStackSize)
   }
 
-  def transCataT[T[_[_]], F[_] : Traverse](tf: T[F])(rewrite: T[F] => T[F], maxStackSize: Int = 200)
-                                                    (implicit Re: Recursive[T], Co: Corecursive[T]): Trampoline[T[F]] = {
-    recLimitStack[T[F], T[F]](tf, (in, loop) => in.project.traverse[Trampoline, T[F]](loop) map (ftf => rewrite(Co.embed[F](ftf))), maxStackSize)
+  @inline def transCataT[T[_[_]], F[_] : Traverse](tf: T[F])(rewrite: T[F] => T[F], maxStackSize: Int = 200)
+                                                  (implicit Re: Recursive[T], Co: Corecursive[T]): Trampoline[T[F]] = {
+    recLimitStack[T[F], T[F]](tf,
+      (in, loop) => in.project.traverse[Trampoline, T[F]](loop) map (ftf => rewrite(Co.embed[F](ftf))), maxStackSize)
   }
 
 }
