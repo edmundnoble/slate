a98acbc60cb5884ffce995fd0a99e8b33d1898d1
Edmund Noble <edmundnoble@gmail.com>
2016-08-12 00:36:53 -0400

    Unsafe

--- qq/shared/src/main/scala/qq/Unsafe.scala
+++ qq/shared/src/main/scala/qq/Unsafe.scala
@@ -1,25 +1,25 @@
 package qq
 
-import scala.collection.mutable
 import scalaz.syntax.applicative._
 import scalaz.{Applicative, Traverse}
 
 object Unsafe {
 
+  // these instances are unsafe because they are for mutable AND immutable collections
+
   implicit final def seqTraverse[T]: Traverse[Seq] = new Traverse[Seq] {
     override def traverseImpl[G[_], A, B](fa: Seq[A])(f: (A) => G[B])(implicit evidence: Applicative[G]): G[Seq[B]] = {
       val bldr = Seq.newBuilder[B]
       bldr.sizeHint(fa)
-      var acc: G[mutable.Builder[B, Seq[B]]] = evidence.point(bldr)
-      var i = 0
-      while (i < fa.length) {
-        val elem: A = fa(i)
+      var acc = evidence.point(bldr)
+      val iter = fa.iterator
+      while (iter.hasNext) {
+        val elem: A = iter.next()
         val comp: G[B] = f(elem)
         acc = (acc |@| comp) { (a, c) =>
           a += c
           a
         }
-        i += 1
       }
       acc.map(_.result())
     }
