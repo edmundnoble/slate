54cbe00ac37c3aed2f822a564a94ee5d92a9db06
Edmund Noble <edmundnoble@gmail.com>
2016-09-07 23:29:22 -0400

    Google auth interactive

--- ui/src/main/scala/dash/app/DashPrelude.scala
+++ ui/src/main/scala/dash/app/DashPrelude.scala
@@ -7,7 +7,8 @@ object DashPrelude {
 
   import CompiledDefinition.noParamDefinition
 
-  def googleAuth: CompiledDefinition[Any] = noParamDefinition("googleAuth", _ => identify.getAuthToken(interactive = false).map(_ :: Nil))
+  def googleAuth: CompiledDefinition[Any] =
+    noParamDefinition("googleAuth", _ => identify.getAuthToken(interactive = true).map(_ :: Nil))
 
   val all = googleAuth +: Vector.empty
 }
