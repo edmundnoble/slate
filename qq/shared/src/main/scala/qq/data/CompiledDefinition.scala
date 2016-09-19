38273e294f4325b1d6a48152c30a293d2da1b212
Edmund Noble <edmundnoble@gmail.com>
2016-09-18 22:36:18 -0400

    Introduced uniform way to handle QQ's function-call semantics

--- qq/shared/src/main/scala/qq/data/CompiledDefinition.scala
+++ qq/shared/src/main/scala/qq/data/CompiledDefinition.scala
@@ -1,7 +1,8 @@
 package qq
 package data
 
-import qq.cc.{CompiledFilter, OrCompilationError, QQCompilationException, UndefinedOnPlatform}
+import monix.eval.Task
+import qq.cc._
 
 import scalaz.syntax.either._
 
@@ -12,11 +13,16 @@ object CompiledDefinition {
   def undefinedOnPlatform[J](name: String): CompiledDefinition[J] =
     CompiledDefinition[J](name, 0, body = _ => UndefinedOnPlatform(name).left)
 
+  // this is responsible for QQ's function application semantics
+  def standardEffectDistribution[J](func: List[J] => Task[J]): List[CompiledFilter[J]] => OrCompilationError[CompiledFilter[J]] =
+    args =>
+      ((b: VarBindings[J]) => (j: J) =>
+        Task.gather(args.map(_ (b)(j))).flatMap(llj => Task.gather(util.foldWithPrefixes(llj.head, llj.tail: _*).map(l => func(l.reverse))))).right
+
   final def noParamDefinition[J](name: String, fun: CompiledFilter[J]): CompiledDefinition[J] = {
     CompiledDefinition[J](
       name,
-      numParams = 0,
-      _ => fun.right[QQCompilationException]
+      numParams = 0, { case Nil => fun.right[QQCompilationException] }
     )
   }
 }
--- ui/src/main/scala/dash/app/DashPrelude.scala
+++ ui/src/main/scala/dash/app/DashPrelude.scala
@@ -1,22 +1,16 @@
 package dash
 package app
 
-import monix.eval.Task
-import monix.scalaz._
 import dash.ajax.{Ajax, AjaxMethod}
+import monix.eval.Task
 import org.scalajs.dom.XMLHttpRequest
-import qq.cc.{QQRuntimeException, VarBindings}
+import qq.cc.QQRuntimeException
 import qq.data.CompiledDefinition
 import qq.jsc._
 import qq.util._
-import upickle.Invalid
 
 import scala.concurrent.duration._
 import scala.scalajs.js
-import scalaz.std.list._
-import scalaz.\/
-import scalaz.syntax.traverse._
-import scalaz.syntax.either._
 
 object DashPrelude {
 
@@ -26,48 +20,43 @@ object DashPrelude {
     noParamDefinition("googleAuth", _ => _ => identify.getAuthToken(interactive = false).map(_ :: Nil))
 
   def launchAuth: CompiledDefinition[Any] =
-    CompiledDefinition[Any]("launchAuth", 1, {
-      case List(urlFilter) =>
-        ((bindings: VarBindings[Any]) => (jsv: Any) =>
-          urlFilter(bindings)(jsv).flatMap(_.head match {
-            case s: String => identify.launchWebAuthFlow(interactive = false, s).map(_ :: Nil)
-            case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not a URL"))
-          })
-          ).right
+    CompiledDefinition[Any]("launchAuth", 1, CompiledDefinition.standardEffectDistribution[Any] {
+      case List(url) =>
+        url match {
+          case s: String => identify.launchWebAuthFlow(interactive = false, s).map(_ :: Nil)
+          case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not a URL"))
+        }
     })
 
-  private def makeAjaxDefinition(name: String, ajaxMethod: AjaxMethod) = CompiledDefinition[Any](name, 4, {
-    case List(urlFilter, queryParamsFilter, dataFilter, headersFilter) => {
-      (bindings: VarBindings[Any]) =>
-        (jsv: Any) =>
-          implicit val ajaxTimeout = Ajax.Timeout(1000.millis)
-          val urlsTask = urlFilter(bindings)(jsv).flatMap(_.traverse {
-            case s: String => Task.now(s)
-            case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not a URL"))
-          })
-          val queryParamssTask = queryParamsFilter(bindings)(jsv).flatMap(_.traverse {
-            case o: js.Object => Task.now(o.toDictionary)
-            case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not query params/data"))
-          })
-          val datasTask = dataFilter(bindings)(jsv).flatMap(_.traverse {
-            case s: String => Task.now(s)
-            case o: js.Object => Task.now(Json.jsToString(o))
-            case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not usable as POST data"))
-          })
-          val headerssTask = headersFilter(bindings)(jsv).flatMap(_.traverse {
-            case o: js.Object => Task.now(o.toDictionary.asInstanceOf[js.Dictionary[String]])
-            case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not headers"))
-          })
-          val ajaxs: Task[List[XMLHttpRequest]] =
-            Task.zipMap4(urlsTask, datasTask, queryParamssTask.map(_.map(_.toMap)), headerssTask.map(_.map(_.toMap)))((urls, datas, queryParamss, headerss) =>
-              for {url <- urls; data <- datas; queryParams <- queryParamss; headers <- headerss} yield
-                Ajax(ajaxMethod, url, data, queryParams, headers, withCredentials = false, "")
-            ).flatMap(_.sequence[Task, XMLHttpRequest])
-          ajaxs.flatMap(
-            _.traverse[Invalid.Json \/ ?, Any](resp => Json.stringToJs(resp.responseText)).fold(Task.raiseError(_), Task.now)
-          )
-    }.right
-  })
+  private def makeAjaxDefinition(name: String, ajaxMethod: AjaxMethod) = CompiledDefinition[Any](name, 4,
+    CompiledDefinition.standardEffectDistribution[Any] {
+      case List(urlRaw, queryParamsRaw, dataRaw, headersRaw) =>
+        implicit val ajaxTimeout = Ajax.Timeout(1000.millis)
+        val urlTask = urlRaw match {
+          case s: String => Task.now(s)
+          case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not a URL"))
+        }
+        val queryParamsTask = queryParamsRaw match {
+          case o: js.Object => Task.now(o.toDictionary.toMap)
+          case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not query params/data"))
+        }
+        val dataTask = dataRaw match {
+          case s: String => Task.now(s)
+          case o: js.Object => Task.now(Json.jsToString(o))
+          case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not usable as POST data"))
+        }
+        val headersTask = headersRaw match {
+          case o: js.Object => Task.now(o.toDictionary.asInstanceOf[js.Dictionary[String]].toMap)
+          case k => Task.raiseError(QQRuntimeException(JSRuntime.print(k) + " is not headers"))
+        }
+        val ajaxs: Task[XMLHttpRequest] =
+          Task.zipMap4(urlTask, dataTask, queryParamsTask, headersTask)((url, data, queryParams, headers) =>
+            Ajax(ajaxMethod, url, data, queryParams, headers, withCredentials = false, "")
+          ).flatten
+        ajaxs.flatMap(
+          resp => Json.stringToJs(resp.responseText).fold(Task.raiseError(_), Task.now)
+        )
+    })
 
   def httpDelete: CompiledDefinition[Any] = makeAjaxDefinition("httpDelete", AjaxMethod.DELETE)
 
