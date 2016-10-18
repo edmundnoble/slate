83c97c23f7ea8ce3478cc0325feb1708469bd08f
Edmund Noble <edmundnoble@gmail.com>
2016-10-17 21:30:13 -0400

    Less print

--- ui/src/main/scala/dash/app/GCalendarApp.scala
+++ ui/src/main/scala/dash/app/GCalendarApp.scala
@@ -9,7 +9,7 @@ qq"""
 def authHeaders: { Authorization: "Bearer " + googleAuth };
 def getEventsOptions: {maxResults: 10, fields: "items(end,location,start,status,summary)",
                        timeMin: nowRFC3339, singleEvents: true, orderBy: "startTime"};
-$$calendarIds as httpGet("https://www.googleapis.com/calendar/v3/users/me/calendarList"; {minAccessRole: "writer"}; {}; authHeaders) | print | .items.[].id in
+$$calendarIds as httpGet("https://www.googleapis.com/calendar/v3/users/me/calendarList"; {minAccessRole: "writer"}; {}; authHeaders) | .items.[].id in
 $$calendarId as $$calendarIds in {
   title: $$calendarId,
   content: httpGet("https://www.googleapis.com/calendar/v3/calendars/" + $$calendarId + "/events";
