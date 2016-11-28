3201d0b159642056d432347b85942f0f5e88dabe
Edmund Noble <edmundnoble@gmail.com>
2016-11-27 23:38:46 -0500

    Less allocation pls

--- ui/src/main/scala/slate/storage/CompressedStorage.scala
+++ ui/src/main/scala/slate/storage/CompressedStorage.scala
@@ -7,12 +7,15 @@ import slate.bindings.LZString
 import scala.util.Try
 
 final case class CompressedStorage[F[_] : Functor](underlying: Storage[F]) extends Storage[F] {
-  override def apply(key: String): F[Option[String]] = underlying(key).map { p =>
-    p.flatMap { r =>
+  override def apply(key: String): F[Option[String]] = underlying(key).map(
+    _.flatMap(r =>
       Try(LZString.decompressFromUTF16(r)).toOption
-    }
-  }
+    )
+  )
 
-  override def update(key: String, data: String): F[Unit] = underlying.update(key, LZString.compressToUTF16(data))
-  override def remove(key: String): F[Unit] = underlying.remove(key)
+  override def update(key: String, data: String): F[Unit] =
+    underlying.update(key, LZString.compressToUTF16(data))
+
+  override def remove(key: String): F[Unit] =
+    underlying.remove(key)
 }
